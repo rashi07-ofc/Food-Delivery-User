c564c80e93451ca4717a3b03a68072ead2b68d5b
Rishabh <rishabh.sharma3@appinventiv.com>
2025-06-11 13:44:51 +0530

    Removed files

---
+++ Food-Delivery-User/src/features/restaurants/RestaurantLandingPage/MainPage.tsx
@@ -0,0 +1,28 @@
+import React, { Suspense, lazy } from "react";
+import Navbar from "./Navbar";
+import RestaurantCard from "./RestaurantCard";
+const RestaurantTabs = lazy(() => import("./RestaurantTabs"));
+const RestaurantHighlights = lazy(() => import("./RestaurantHighlights"));
+const Footer = lazy(() => import("./Footer"));
+import { restaurant } from "./restaurantData";
+import FoodLoader from "./FoodLoader";
+
+const MainPage: React.FC = () => {
+  return (
+    <>
+      <div className="max-w-6xl mx-auto px-6 md:px-12 lg:px-20 pb-12">
+        <Navbar />
+        <RestaurantCard />
+        <Suspense fallback={<FoodLoader />}>
+          <div id="restaurant-tabs">
+            <RestaurantTabs />
+          </div>
+          <RestaurantHighlights features={restaurant.features} />
+        </Suspense>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default MainPage;
---
+++ Food-Delivery-User/src/routes/AppRoutes.tsx
@@ -0,0 +1,39 @@
+import { Routes, Route } from "react-router-dom";
+// import AdminDashboard from "../features/admin/pages/AdminDashboard";
+// import ManageManagers from "../features/admin/pages/AdminManagerManager";
+// import SpecialOrders from "../features/admin/pages/AdminSpecialOrders";
+// import SpecialOrderDetails from "../features/admin/pages/SpecialOrderDetails";
+// import NotFound from "../features/admin/pages/NotFound";
+import MainPage from "../features/restaurants/RestaurantLandingPage/MainPage";
+import DelieveryLanding from "../features/restaurants/Delivery/DeliveryLanding";
+import Checkout from "../features/cart/Checkout";
+import CartPage from "../features/cart/CartPage";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      {/* Admin Routes
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/manage-managers" element={<ManageManagers />} />
+      <Route path="/admin/special-orders" element={<SpecialOrders />} /> */}
+
+      {/*For view order details:--_*/}
+      {/* <Route path="/special-orders/:orderId" element={<SpecialOrderDetails />} /> */}
+      {/* <Route
+        path="/admin/special-orders/:orderId"
+        element={<SpecialOrderDetails />}
+      /> */}
+     
+      {/* Restaurant Routes */}
+      <Route path="/restaurant/landing" element={<MainPage />} />
+      <Route path="/restaurant/delivery" element={<DelieveryLanding />} />
+
+      {/* Cart Routes */}
+      <Route path="/cart/checkout" element={<Checkout />} />
+      <Route path="/cart" element={<CartPage />} />
+
+      {/* Fallback */}
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  );
+}
